6539e00f33d060ebae204151f55243f7d0676a62
Waradu <waradu@outlook.com>
2025-06-07 21:55:23 +0200

    feat: screenshot

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
-import { LucideTerminal } from "lucide-vue-next";
+import {
+  LucideLayoutGrid,
+  LucideTerminal,
+  type LucideProps,
+} from "lucide-vue-next";
+import type { FunctionalComponent } from "vue";
+
+interface Game {
+  name: string;
+  icon: FunctionalComponent<LucideProps, {}, any, {}>;
+  href: string;
+}
+
+const games: Game[] = [
+  {
+    name: "Wordle",
+    icon: LucideLayoutGrid,
+    href: "/wordle",
+  },
+];
 </script>
 
 <template>
   <div
-    class="bg-neutral-950 text-xl text-white w-full h-full flex gap-4 items-center justify-center cursor-default select-none"
+    class="bg-neutral-900 w-full min-h-full h-max flex flex-col gap-2 items-center justify-center select-none"
   >
-    <LucideTerminal />
-    <span class="mb-1.5">Nuxt Template</span>
+    <NuxtLink
+      class="flex gap-4 text-neutral-200 items-center bg-neutral-800 hover:bg-neutral-700 active:bg-neutral-700 hover:text-white active:text-white transition p-2 px-4 rounded-md"
+      v-for="game in games"
+      :to="game.href"
+    >
+      <component :is="game.icon" class="size-5" />
+      <span class="text-xl">{{ game.name }}</span>
+    </NuxtLink>
   </div>
 </template>
--- pages/wordle.vue
+++ pages/wordle.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full min-h-full h-max dark flex flex-col gap-8 items-center p-8 px-4 bg-neutral-900 text-neutral-200 select-none"
+    class="w-full min-h-full h-max dark flex flex-col gap-4 items-center p-8 px-4 bg-neutral-900 text-neutral-200 select-none"
   >
     <h1 class="text-4xl font-bold flex items-center gap-4">
       <span>Wordle</span>
@@ -8,48 +8,65 @@
         class="p-2 bg-neutral-800 active:bg-neutral-700 hover:bg-neutral-700 rounded-md"
         @click="reset"
       >
-        <LucideRotateCcw class="size-4" />
+        <LucideRotateCcw class="size-4 text-neutral-200" />
+      </div>
+      <div
+        class="p-2 bg-neutral-800 active:bg-neutral-700 hover:bg-neutral-700 rounded-md"
+        @click="screenshot"
+      >
+        <LucideCamera class="size-4 text-neutral-200" />
       </div>
     </h1>
-    <div class="flex flex-col gap-2 perspective-1000">
-      <div v-for="row in guessCount" class="flex gap-2">
-        <div
-          v-for="column in lettersCount"
-          class="relative preserve-3d perspective-1000 size-12 lg:size-16"
-          :id="`box-${row}-${column}`"
-        >
+    <div ref="boardRef" class="bg-neutral-900 p-4 flex flex-col gap-4">
+      <div
+        class="text-neutral-400 flex justify-between text-sm"
+        v-if="screenshotting"
+      >
+        <span class="">https://games.waradu.dev</span>
+        <span class="text-neutral-200" v-if="state != GameState.PLAYING">
+          "{{ word.toUpperCase() }}"
+        </span>
+      </div>
+      <div class="flex flex-col gap-2 perspective-1000">
+        <div v-for="row in guessCount" class="flex gap-2">
           <div
-            class="absolute inset-0 flex items-center justify-center text-xl lg:text-2xl border-4 border-neutral-800 bg-transparent text-neutral-400 rounded-xl backface-hidden"
-            :class="`reset row-${row}`"
+            v-for="column in lettersCount"
+            class="relative preserve-3d perspective-1000 size-12 lg:size-16"
+            :id="`box-${row}-${column}`"
           >
-            {{
-              row - 1 == currentLine
-                ? currentText[column - 1]?.toUpperCase()
-                : currentLine >= row
-                ? guesses[row - 1][column - 1]?.toUpperCase()
-                : ""
-            }}
-          </div>
-          <div
-            class="absolute inset-0 flex items-center justify-center text-white text-2xl rounded-xl rotate-x-180 transform backface-hidden font-bold"
-            :class="[
-              `reset-back row-${row}-back`,
-              currentLine >= row
-                ? guesseResults[row - 1][column - 1] == LetterStatus.CORRECT
-                  ? 'bg-[#568637]'
-                  : guesseResults[row - 1][column - 1] == LetterStatus.PRESENT
-                  ? 'bg-[#b99c49]'
-                  : 'bg-neutral-800'
-                : '',
-            ]"
-          >
-            {{
-              row - 1 == currentLine
-                ? currentText[column - 1]?.toUpperCase()
-                : currentLine >= row
-                ? guesses[row - 1][column - 1]?.toUpperCase()
-                : ""
-            }}
+            <div
+              class="absolute inset-0 flex items-center justify-center text-xl lg:text-2xl border-4 border-neutral-800 bg-transparent text-neutral-200 rounded-xl backface-hidden"
+              :class="`reset row-${row}`"
+            >
+              {{
+                row - 1 == currentLine
+                  ? currentText[column - 1]?.toUpperCase()
+                  : currentLine >= row
+                  ? guesses[row - 1][column - 1]?.toUpperCase()
+                  : ""
+              }}
+            </div>
+            <div
+              class="absolute inset-0 flex items-center justify-center text-white text-2xl rounded-xl rotate-x-180 transform backface-hidden font-bold"
+              :class="[
+                `reset-back row-${row}-back`,
+                currentLine >= row
+                  ? guesseResults[row - 1][column - 1] == LetterStatus.CORRECT
+                    ? 'bg-[#568637]'
+                    : guesseResults[row - 1][column - 1] == LetterStatus.PRESENT
+                    ? 'bg-[#b99c49]'
+                    : 'bg-neutral-800'
+                  : '',
+              ]"
+            >
+              {{
+                row - 1 == currentLine
+                  ? currentText[column - 1]?.toUpperCase()
+                  : currentLine >= row
+                  ? guesses[row - 1][column - 1]?.toUpperCase()
+                  : ""
+              }}
+            </div>
           </div>
         </div>
       </div>
@@ -63,41 +80,39 @@
         }"
       >
         <div
-				  v-for="key in row"
-				  :key="key"
-				  class="h-12 flex justify-center items-center rounded-xl transition-colors cursor-pointer"
-				  :class="[
-				    keyStatuses.get(key) !== undefined
-				      ? keyStatuses.get(key) === LetterStatus.CORRECT
-				        ? 'bg-[#568637] text-white'
-				        : keyStatuses.get(key) === LetterStatus.PRESENT
-				        ? 'bg-[#b99c49] text-white'
-				        : 'bg-neutral-800 text-white'
-				      : 'bg-neutral-700 hover:bg-neutral-600 active:bg-neutral-600 text-neutral-200'
-				  ]"
-				  @click="addKey(key)"
-				>
-				  {{ key.toUpperCase() }}
-				</div>
+          v-for="key in row"
+          :key="key"
+          class="h-12 flex justify-center items-center rounded-xl transition-colors"
+          :class="[
+            keyStatuses.get(key) !== undefined
+              ? keyStatuses.get(key) === LetterStatus.CORRECT
+                ? 'bg-[#568637] text-white'
+                : keyStatuses.get(key) === LetterStatus.PRESENT
+                ? 'bg-[#b99c49] text-white'
+                : 'bg-neutral-800 text-white'
+              : 'bg-neutral-700 hover:bg-neutral-600 active:bg-neutral-600 text-neutral-200',
+          ]"
+          @click="addKey(key)"
+        >
+          {{ key.toUpperCase() }}
+        </div>
       </div>
     </div>
     <div
-      class="flex flex-col items-center justify-center absolute inset-0 z-10 bg-neutral-900/80 backdrop-blur-xs text-md gap-4"
+      class="flex flex-col items-center justify-center absolute inset-0 z-10 bg-neutral-900/80 backdrop-blur-sm text-md gap-4"
       v-if="modalOpen"
     >
-      <div v-if="state != GameState.PLAYING">{{ `The answer was: ${word}` }}</div>
-      <div
-        class="bg-neutral-800 rounded-xl cursor-pointer p-4 px-8 text-xl"
-        v-if="state != GameState.PLAYING"
-        @click="reset"
-      >
+      <div v-if="state != GameState.PLAYING">
+        {{ `The answer was: ${word}` }}
+      </div>
+      <div class="bg-neutral-800 rounded-xl p-4 px-8 text-xl" @click="reset">
         New Game
       </div>
       <div
-        class="bg-neutral-800 rounded-xl cursor-pointer p-4 px-8 text-xl"
+        class="p-2 bg-neutral-800 active:bg-neutral-700 hover:bg-neutral-700 rounded-md absolute right-2 top-2"
         @click="modalOpen = false"
       >
-        Close
+        <LucideX class="size-4 text-neutral-200" />
       </div>
     </div>
   </div>
@@ -109,7 +124,37 @@ import ExtraWords from "~/assets/wordle/extra_words.txt?raw";
 import Characters from "~/assets/wordle/characters.txt?raw";
 import { animate, stagger } from "motion-v";
 import { Key } from "@waradu/keyboard";
-import { LucideRefreshCcw, LucideRotateCcw } from "lucide-vue-next";
+import { LucideCamera, LucideRotateCcw, LucideX } from "lucide-vue-next";
+import { toPng } from "html-to-image";
+
+useHead({
+  title: "Wordle",
+});
+
+const boardRef = ref<HTMLElement | null>(null);
+const screenshotting = ref(false);
+
+const screenshot = async () => {
+  if (!boardRef.value) return;
+  screenshotting.value = true;
+  setTimeout(async () => {
+    try {
+      if (!boardRef.value) return;
+
+      const dataUrl = await toPng(boardRef.value, {
+        skipFonts: true,
+      });
+      screenshotting.value = false;
+
+      const a = document.createElement("a");
+      a.href = dataUrl;
+      a.download = "board.png";
+      a.click();
+    } catch (e) {
+      console.error("screenshot failed", e);
+    }
+  }, 1);
+};
 
 const keyboard = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
@@ -224,17 +269,23 @@ const deleteChar = () => {
 $keyboard.listen([Key.Backspace], deleteChar);
 
 const submit = () => {
+  if (state.value != GameState.PLAYING) {
+    reset();
+    return;
+  }
+
   if (
     lettersCount != currentText.value.length ||
     !(
       words.includes(currentText.value) ||
       extraWords.includes(currentText.value)
-    )
+    ) ||
+    guesses.value.includes(currentText.value)
   ) {
     animate(
       `.row-${currentLine.value + 1}`,
-      { rotate: [0, -5, 10, -5, 0] },
-      { duration: 0.4, ease: "easeInOut" }
+      { rotate: [0, -5, 10, -5, 10, -5, 0], opacity: [1, 0.5, 1] },
+      { duration: 0.6, ease: "easeInOut" }
     );
     return;
   }
@@ -264,7 +315,6 @@ const submit = () => {
   if (result.every((res) => res == LetterStatus.CORRECT)) {
     confetti();
     state.value = GameState.WON;
-    modalOpen.value = true;
     return;
   }
   if (currentLine.value == guessCount) {
@@ -288,7 +338,7 @@ const reset = () => {
   guesses.value = [];
   state.value = GameState.PLAYING;
   word.value = words[Math.floor(Math.random() * words.length)];
-	modalOpen.value = false;
+  modalOpen.value = false;
 
   animate(`.reset`, { rotateX: 0 }, { duration: 0 });
 
