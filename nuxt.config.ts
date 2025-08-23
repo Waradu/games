fea1f073f963e78f41b77b4c88f56d2f76eb6a3a
Waradu <waradu@outlook.com>
2025-08-23 22:12:53 +0200

    feat: new name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -116,12 +116,12 @@ export default defineNuxtConfig({
       orientation: "portrait",
       icons: [
         {
-          src: "/pwa-192x192.png",
+          src: "/192x192.pwa.png",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "/pwa-512x512.png",
+          src: "/512x512.pwa.png",
           sizes: "512x512",
           type: "image/png",
         },
